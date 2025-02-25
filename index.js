ca95e37d39f60e88cad4b2eb9ee6ed2a6344d382
Maximiliano Cerra <cerramaximiliano@gmail.com>
2025-02-25 12:56:00 -0300

    Corrección de permisos

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports = {
+    CausasCivil: require('./src/models/causas-civil'),
+    CausasSegSoc: require("./src/models/causas-ss"),
+    CausasTrabajo: require("./src/models/causas-trabajo"),
+    ConfiguracionScraping: require("./src/models/configuration_scraping"),
+};
